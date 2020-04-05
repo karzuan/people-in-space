fdb94c98ff287af95a6f7d9803677f1925ff0ad9
Konstantin Kuts <karzuan@gmail.com>
2020-04-05 10:46:56 +0800

    Error Handling with try...catch

--- js/async-await.js
+++ js/async-await.js
@@ -3,10 +3,22 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
+// Hangle the errors
+
+async function getJSON(url) {
+  try{
+    const response = await fetch (url);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 // Handle all fetch requests
 async function getPeopleInSpace(url){
-  const peopleResponse = await fetch(url);
-  const peopleJSON = await peopleResponse.json();
+  //const peopleResponse = await getJSON(url);
+  const peopleJSON = await getJSON(url);
 
   const profiles = peopleJSON.people.map( async (person) => {
       const craft = person.craft;
@@ -14,8 +26,7 @@ async function getPeopleInSpace(url){
       {
         person.name = "Andrew R. Morgan";
       } 
-      const profileResponse = await fetch(wikiUrl + person.name);
-      const profileJSON = await profileResponse.json();
+      const profileJSON = await getJSON( wikiUrl + person.name);
 
       return{...profileJSON, craft };
   });
@@ -43,6 +54,10 @@ btn.addEventListener('click', (event) => {
 
   getPeopleInSpace(astrosUrl)
     .then (generateHTML)
+    .catch( e=> {
+      peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+      console.error(e);
+    })
     .finally( () => event.target.remove() );
 
 });
